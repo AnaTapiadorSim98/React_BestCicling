7fe0d51def2cd0332148fd4ba2306c6a51082348
AnaTapiadorSim98 <72818044+AnaTapiadorSim98@users.noreply.github.com>
2021-10-09 17:55:54 +0200

    Revert "Error no abre pagina"
    This reverts commit 2a4d2194bf877e98431319dd25bfec4e777d9a13.

--- my-app/src/FirstPage/FirstPage.jsx
+++ my-app/src/FirstPage/FirstPage.jsx
@@ -1,79 +1,34 @@
-import '../App.css';
-import React from 'react';
-import Header from '../Header';
 
-import ReactDOM from "react-dom";
-
-function obtener_instructor(numero) {
-  var i = 0;
-  while (1 < Object.keys(customData.instructors).length) {
-    if (numero == customData.instructors[i].id) {
-      return customData.instructors[i].name;
-    }
-    i++;
-  }
-}
-
-const customData = require('../db.json');
-
-
-function genera_tabla() {
-
-  
-  var body = document.getElementsByTagName("body")[0];
-  //var body = document.getElementsByTagName("app")[0];
-  var tabla   = document.createElement("table");
-  var tblBody = document.createElement("tbody");
- 
-  // Crea las celdas
-  for (var i = 0; i < 2; i++) {
-    // Crea las hileras de la tabla
-    var hilera = document.createElement("tr");
-    for (var j = 0; j < 2; j++) {
-      // Crea un elemento <td> y un nodo de texto, haz que el nodo de
-      // texto sea el contenido de <td>, ubica el elemento <td> al final
-      // de la hilera de la tabla
-      var celda = document.createElement("td");
-      var textoCelda = document.createTextNode("celda en la hilera "+i+", columna "+j);
-      celda.appendChild(textoCelda);
-      hilera.appendChild(celda);
-    }
-    // agrega la hilera al final de la tabla (al final del elemento tblbody)
-    tblBody.appendChild(hilera);
-  }
-  // posiciona el <tbody> debajo del elemento <table>
-
-  while(tabla.hasChildNodes){
-    tabla.remove();
-  }
-
-  tabla.appendChild(tblBody);
-  //tabla.removeChild(tblBody)
-  while(body.hasChildNodes){
-    body.remove();
-  }
-  body.appendChild(tabla);
-  // modifica el atributo "border" de la tabla y lo fija a "2";
-  tabla.setAttribute("border", "2");
-
-
-}
+/*import React from 'react';
+import ReactDOM from 'react-dom'
 
 function FirstPage() {
+  const element = (
   
-  return (
-    
-    <div className="App" id="App" tagName="app">
-      <Header />
-     {genera_tabla()}
-    </div> 
-  )
-  
-  ;
+    <div>aaaaa</div>
+  );
+ // ReactDOM.render(element, document.getElementById('root'));
 }
 
 
 
-//<script >{makeGrid()}</script>
-//<table id="Tabla-Clases-Dinamicas"></table>
+export default FirstPage;*/
+
+
+import React from 'react';
+
+
+//import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+function FirstPage() {
+  return (
+
+    <div >Hola Mundo </div>
+
+  );
+
+}
+
+
 export default FirstPage;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -9,23 +9,15 @@ import "./index.css";
 //import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 //import FirstPage from './FirstPage';
-import history from "./history";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router history={history}>
+    <Router>
       <App />     
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
-//export default function Login(props) {
- // props.history.push('/home')
-//}
-
-export default function Login() {
-  history.push('/home')
-}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
